178c6ea45ee68e64444b49b180ff646743aa54f8
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2024-05-31 11:17:17 +0300

    feat: small update to .stories.js file

--- stories/cosmoz-slider.stories.js
+++ stories/cosmoz-slider.stories.js
@@ -1,17 +1,16 @@
+import '../demo/basic-slider';
+import '../demo/tab-slider';
+import '../demo/advanced-slider';
+import { html } from '@pionjs/pion';
+
 export default {
 	title: 'Cosmoz Slider',
 };
 
-import '../demo/basic-slider';
-import '../demo/tab-slider';
-import '../demo/advanced-slider';
-import { html } from 'lit-html';
+export const BasicSlider = () => html`<demo-basic-slider></demo-basic-slider>`;
 
-const BasicSlider = () => '<demo-basic-slider></demo-basic-slider>',
-	TabSlider = () => '<demo-tab-slider></demo-tab-slider>',
-	AdvancedSlider = ({ loop }) =>
-		html`<demo-advanced-slider ?loop=${loop}></demo-advanced-slider>`;
+export const TabSlider = () => html`<demo-tab-slider></demo-tab-slider>`;
 
+export const AdvancedSlider = ({ loop }) =>
+	html`<demo-advanced-slider ?loop=${loop}></demo-advanced-slider>`;
 AdvancedSlider.args = { loop: false };
-
-export { BasicSlider, TabSlider, AdvancedSlider };
