4e5ae1da3fd10459468e374b3079b14a24a0db36
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2024-06-03 09:49:59 +0300

    feat: add autodocs to storybook

--- stories/cosmoz-slider.stories.js
+++ stories/cosmoz-slider.stories.js
@@ -5,12 +5,34 @@ import { html } from '@pionjs/pion';
 
 export default {
 	title: 'Cosmoz Slider',
+	tags: ['autodocs'],
 };
 
 export const BasicSlider = () => html`<demo-basic-slider></demo-basic-slider>`;
+BasicSlider.parameters = {
+	docs: {
+		description: {
+			story: 'The basic version of Slider',
+		},
+	},
+};
 
 export const TabSlider = () => html`<demo-tab-slider></demo-tab-slider>`;
+TabSlider.parameters = {
+	docs: {
+		description: {
+			story: 'The tab version of Slider',
+		},
+	},
+};
 
 export const AdvancedSlider = ({ loop }) =>
 	html`<demo-advanced-slider ?loop=${loop}></demo-advanced-slider>`;
 AdvancedSlider.args = { loop: false };
+AdvancedSlider.parameters = {
+	docs: {
+		description: {
+			story: 'The advanced version of Slider',
+		},
+	},
+};
