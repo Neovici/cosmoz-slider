f42f1e626b020ee8797039c7b7e0af29dbd74729
basilgood <elsile69@yahoo.com>
2025-09-10 21:34:40 +0000

    Deploying to gh-pages from @ Neovici/cosmoz-slider@b6ddbed928479fd3f604bcb9cbf5c7afd47720b5 🚀

--- entry-preview-izj0ky5T.js
+++
@@ -1,7 +0,0 @@
-export { r as render, a as renderToCanvas } from './chunk-FJPRWHXQ-0gSPYeaT.js';
-import './index-Kjm4kNkQ.js';
-import './directive-helpers-BsidKJLT.js';
-
-var parameters={renderer:"web-components"};
-
-export { parameters };
